c0536a2e2a76172c51fab3a6449b6c8779c1aaa4
Jonathan Baldie <jon@jonbaldie.com>
2022-09-22 07:19:17 +0100

    null test added

--- tests/QuickQueryTest.php
+++ tests/QuickQueryTest.php
@@ -60,4 +60,49 @@ class QuickQueryTest extends \PHPUnit\Framework\TestCase
 
         $this->assertEquals($expected, $result);
     }
+
+    /**
+     * @test
+     */
+    public function it_handles_nulls_from_sql()
+    {
+        try {
+            $pdo = new \PDO('sqlite::memory:');
+        } catch (\PDOException $error) {
+            $this->markTestSkipped($error->getMessage());
+        }
+
+        $connection = new \TypeDb\Connection($pdo);
+
+        \TypeDb\quick_query(
+            $connection,
+            'create table if not exists type_db_null ( id int not null, value varchar null )',
+            []
+        );
+
+        \TypeDb\quick_query(
+            $connection,
+            'insert into type_db_null (id, value) values (?, ?), (?, ?)',
+            [\TypeDb\to_sql(1), \TypeDb\to_sql('bar'), \TypeDb\to_sql(2), \TypeDb\to_sql(null)]
+        );
+
+        $result = \TypeDb\quick_query(
+            $connection,
+            "select id, value from type_db_null where id = ? or value is null",
+            [\TypeDb\to_sql(1)]
+        );
+
+        $expected = [
+            [
+                'id' => new \TypeDb\SqlValue\SqlInteger(1),
+                'value' => new \TypeDb\SqlValue\SqlString('bar'),
+            ],
+            [
+                'id' => new \TypeDb\SqlValue\SqlInteger(2),
+                'value' => new \TypeDb\SqlValue\SqlNull(),
+            ],
+        ];
+
+        $this->assertEquals($expected, $result);
+    }
 }
