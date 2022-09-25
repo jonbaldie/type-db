7d7fd3551a223d1d394e842d0efe0bfbc511decf
Jonathan Baldie <jon@jonbaldie.com>
2022-09-25 11:05:28 +0100

    Connection::quickQuery() method

--- src/Connection.php
+++ src/Connection.php
@@ -11,4 +11,15 @@ class Connection
     public function __construct(
         public readonly PDO $pdo
     ) {}
+
+    /**
+     * @param string $sql
+     * @param SqlValue\SqlValue[] $sql_values
+     * @return SqlValue\SqlValue[][]
+     */
+    public function quickQuery(
+        string $sql, array $sql_values
+    ): array {
+        return quick_query($this, $sql, $sql_values);
+    }
 }
